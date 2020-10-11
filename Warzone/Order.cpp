856e161980de266732bb8f598653342d3cf9abe1
Erix-F5R <49351754+Erix-F5R@users.noreply.github.com>
2020-10-11 15:15:53 -0400

    Update Order.cpp

--- Warzone/Order.cpp
+++ Warzone/Order.cpp
@@ -1,23 +1,51 @@
 #include "Order.h"
 #include <iostream>
 #include <fstream>
+#include <typeinfo>
+#include <vector>
+#include <list>
 
-Order::Order() {};
+Order::Order() { setName("Order"); };
 
 bool Order::validate() { return true; };
-bool Order::execute() { return validate(); };
+bool Order::execute() { return validate(); }
+void Order::setName(string n) { name = n;}
+string Order::getName() { return name; }
 
-Deploy::Deploy() { cout << "Created a deploy order\n"; };
+bool Order::operator== (Order o1)
+{
+    return (getName() == o1.getName() );
+}
 
-Advance::Advance() { cout << "Created an advance order\n"; };
 
-Bomb::Bomb() { cout << "Created a bomb order\n"; };
+std::ostream& operator<<(std::ostream& strm, Order t)
+{
+	return strm << t.name;
+};
 
-Blockade::Blockade() { cout << "Created a blockade order\n"; };
+Deploy::Deploy() { 
+	setName("Deploy");
+	cout << "Created a deploy order\n"; };
 
-Airlift::Airlift() { cout << "Created an airlift order\n"; };
+Advance::Advance() { 
+	setName("Advance");
+	cout << "Created an advance order\n"; };
 
-Negotiate::Negotiate() { cout << "Created a negotiate order\n"; };
+Bomb::Bomb() { 
+	setName("Bomb");
+	cout << "Created a bomb order\n"; };
+
+Blockade::Blockade() { 
+	setName("Blockade");
+	cout << "Created a blockade order\n"; };
+
+Airlift::Airlift() { 
+	setName("Airlift");
+	cout << "Created an airlift order\n"; };
+
+Negotiate::Negotiate() { 
+	setName("Negotiate");
+	cout << "Created a negotiate order\n"; };
 
 Orderlist::Orderlist() { };
 
@@ -27,12 +55,37 @@ void Orderlist::add(Order order) {
 		
 }
 
-Order Orderlist::Delete(Order order) {
+Order Orderlist::remove(Order order) {
 	//TODO
-	//Find order in list and delete then return
+
+	for (auto it = orderList.begin(); it != orderList.end(); it++)
+		if (*it == order) {
+			orderList.erase(it);
+			return order;
+		}
+	
 	return order;
+
 }
 
-void Orderlist::move(Order order) {
+
+void Orderlist::move(Order insertOrder, Order destinationOrder) {
 	//TODO
+
+	auto insert = std::find(orderList.begin(), orderList.end(), insertOrder);
+	
+	auto destination = std::find(orderList.begin(), orderList.end(), destinationOrder);
+
+	orderList.splice(destination, orderList, insert);
+
 }
+
+std::ostream& operator<<(std::ostream& strm, Orderlist t)
+{	
+	
+	for (auto it = t.orderList.begin(); it != t.orderList.end(); it++)
+		strm << *it << " ";
+	
+	return strm;
+};
+
