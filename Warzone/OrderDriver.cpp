9305dcc0045ba64264638753ab8b059f0a5aeaf5
Erix-F5R <49351754+Erix-F5R@users.noreply.github.com>
2020-10-11 15:16:31 -0400

    Update OrderDriver.cpp

--- Warzone/OrderDriver.cpp
+++ Warzone/OrderDriver.cpp
@@ -3,21 +3,42 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-#include <list>
+#include <vector>
 
 int main() {
 	Order o1 = Order();
 	Orderlist olist = Orderlist();
 
-	Bomb b1 = Bomb();
+	
 
-	Airlift a1 = Airlift();
+	Airlift airlift1 = Airlift();
+	Bomb bomb1 = Bomb();
+	Deploy deploy1 = Deploy();
+	Advance adv1 = Advance();
+	Blockade block1 = Blockade();
+	Negotiate nego1 = Negotiate();
 
-	olist.add(a1);
-	olist.add(b1);
+	olist.add(airlift1);
+	olist.add(bomb1);
+	olist.add(block1);
+	olist.add(adv1);
+	olist.add(nego1);
+	olist.add(deploy1);
+
+	cout << olist << "\n";
+
+	olist.move(nego1, block1);
+
+	cout << olist << "\n";
+
+	cout << o1;
+
+
+	
 
 	
 
 
 }
+
 */
