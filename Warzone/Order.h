712b6384561726b3976e3ff267c9e2ec88f8e9c4
Erix-F5R <49351754+Erix-F5R@users.noreply.github.com>
2020-10-12 14:39:57 -0400

    Update Order.h

--- Warzone/Order.h
+++ Warzone/Order.h
@@ -1,77 +1,142 @@
+/*COMP 345 A1
+  Team 21
+*/
 using namespace std;
-#include <vector>
 #include <iostream>
 #include <list>
 
-
+//Parents class Order
+//Currently has no attributes implemented
 class Order {
     
     string name;
 
+//Included Copy, Assignment, Outstream
 public:    
     Order();
+    Order(const Order& o);
     bool validate();
     bool execute();
     void setName(string n);
     string getName();
-    friend std::ostream& operator<<(std::ostream&, const Order);
-    bool operator== (Order o1);
+    friend std::ostream& operator<<(std::ostream& strm, Order& o);
+    Order& operator= (const Order& o);
 
 };
 
 
 
-//Subclasses deploy, advance, bomb, blockade, airlift, and negotiate.
+//Deploy
 class Deploy : public Order {
+
+    string name;
 
 public:
     Deploy();
+    Deploy(const Deploy& o);
+
+    bool validate();
+    bool execute();
+
+    Deploy& operator= (const Deploy& o);
    
 };
+
+//Advance
 class Advance : public Order {
+    
+    string name;
   
 public:
     Advance();
+    Advance(const Advance& o);
+
+    bool validate();
+    bool execute();
+
+    Advance& operator= (const Advance& o);
 
 };
+
+//Bomb
 class Bomb : public Order {
+
+    string name;
 
 public:
     Bomb();
+    Bomb(const Bomb& o);
+
+    bool validate();
+    bool execute();
+
+    Bomb& operator= (const Bomb& o);
 };
+
+//Blockade
 class Blockade : public Order {
+
+    string name;
     
 public:
     Blockade();
+    Blockade(const Blockade& o);
+
+    bool validate();
+    bool execute();
+
+    Blockade& operator= (const Blockade& o);
 };
+
+//Airlift
 class Airlift : public Order {
+
+    string name;
     
 public:
     Airlift();
+    Airlift(const Airlift& o);
+
+    bool validate();
+    bool execute();
+
+    Airlift& operator= (const Airlift& o);
 };
+
+//Negotiate
 class Negotiate : public Order {
+
+    string name;
     
 public:
     Negotiate();
+    Negotiate(const Negotiate& o);
+
+    bool validate();
+    bool execute();
+
+    Negotiate& operator= (const Negotiate& o);
 };
 
 
-
+//Orderlist points to a <list> of Order pointers
 class Orderlist {
 
-    list <Order> orderList;
+    list <Order*>* orderList;
 
 public:
     
     Orderlist();
 
-    void move(Order insertOrder, Order destinationOrder);
+    Orderlist(const Orderlist& ol);
 
-    Order remove(Order order);
+    void move(Order* insertOrder, Order* destinationOrder);
 
-    void add(Order order);
+    Order remove(Order* order);
 
-    friend std::ostream& operator<<(std::ostream&, const Orderlist);
+    void add(Order* order);
+
+   friend std::ostream& operator<<(std::ostream& strm, const Orderlist& ol);
 
 
 };
