2104611aadf4694a882c17a33e01862758754719
Raymart De Guzman <tramyardg@gmail.com>
2020-10-18 19:03:53 -0400

    minor fix MapLoaderDriver

--- Warzone/MapLoaderDriver.cpp
+++ Warzone/MapLoaderDriver.cpp
@@ -1,24 +1,29 @@
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include "MapLoader.h"
-//#include "Map.h"
-//using namespace std;
-//
-//int main() {
-//
-//	string canadaMapFile = "maps/canada.map";
-//	MapLoader canadaMap = MapLoader();
-//	canadaMap.setFileName(canadaMapFile);
-//	canadaMap.storeContinents();
-//	canadaMap.storeTerritories();
-//	canadaMap.storeTerritoriesWithBorders();
-//	Map canadaCreatedMap = Map(canadaMap.getTerritories().size() + 1, true);
-//
-//	for (size_t i = 0; i < canadaMap.getTerritoriesWithBorders().size(); i++) {
-//		canadaCreatedMap.addEdge(canadaMap.getTerritoriesWithBorders()[i]);
-//	}
-//	canadaCreatedMap.displayAdjacencyList();
-//	canadaCreatedMap.validate();
-//}
-//
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "MapLoader.h"
+#include "Map.h"
+using namespace std;
+
+int main() {
+
+	// load and store map objects
+	string canadaMapFile = "maps/canada.map";
+	MapLoader canadaMap = MapLoader();
+	canadaMap.setFileName(canadaMapFile);
+	canadaMap.storeContinents();
+	canadaMap.storeTerritories();
+	canadaMap.storeTerritoriesWithBorders();
+	
+	// create map and validate
+	Map* map;
+	map = new Map();
+	map->createMap(canadaMap.getTerritories().size() + 1, true);
+
+	for (size_t i = 0; i < canadaMap.getTerritoriesWithBorders().size(); i++) {
+		map->addEdge(canadaMap.getTerritoriesWithBorders()[i]);
+	}
+	map->displayAdjacencyList();
+	map->validate();
+}
+
